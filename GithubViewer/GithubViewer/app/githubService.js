f656e2015e894dee112e0168befef4523b80872f
Dmitry Gorshkov <goodvim@gmail.com>
2015-04-30 23:21:56 +0300

    angular app, githubService, userSearch

---
+++ GithubViewer/GithubViewer/app/githubService.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    angular.module('githubViewerApp')
+    .factory('githubService', ['$http', githubService]);
+
+    function githubService($http) {
+        return {
+            getUser: getUser,
+            getRepo: getRepo
+        };
+
+        function getUser(username) {
+            return $http.get('https://api.github.com/users/' + username, onUserSuccess);
+        };
+
+        function onUserSuccess(response) {
+            return response.data;
+        };
+
+        function onRepo() {
+
+        };
+    };
+})();
---
+++ GithubViewer/GithubViewer/app/userSearch/userSearchController.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    angular.module('githubViewerApp')
+    .controller('useSearchController', ['$scope', '$location', githubController]);
+
+    function userSearchController($scope, $location) {
+        function search() {
+            $location.path('/user/' + $scope.login);
+        }
+    }
+})();
